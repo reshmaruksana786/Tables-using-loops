581172e7713847289bcc68effcce2b866bc5cdb4
Syed.Reshma Ruksana <reshmasmile468@achiever-107.local>
2019-08-03 09:47:59 +0530

    Initial Commit

---
+++ 1970-Tables using loops/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1970-Tables using loops
+//
+//  Created by Syed.Reshma Ruksana on 8/3/19.
+//  Copyright © 2019 Syed.Reshma Ruksana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
